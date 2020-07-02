2acddea9013de03a4cc16d97916ae5ba54ebfd4c
Peep Pirnpuu <peep@pirnpuu.ee>
2020-07-02 23:24:22 +0300

    Add StrictMode comment

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,10 @@ import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducers);
 
+// StrictMode renders components twice (on dev but not production) in
+// order to detect any problems with your code and warn you about them
+// (which can be quite useful).
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
